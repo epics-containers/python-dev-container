2a411e065c51958a20d95418c1cacf05494a6703
Callum Forrester <callum.forrester@diamond.ac.uk>
2021-10-19 10:38:44 +0100

    Move configuration out of build and rely on mounting home directory

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -f /scratch ]; then 
+if [ -d /scratch ]; then 
    workdir=/scratch/${USER}/work
 else
    workdir=/home/${USER}/work
@@ -10,12 +10,12 @@ mkdir -p ${workdir}
 echo "Using ${workdir}"
 
 image=python-dev:latest
-environ="-e DISPLAY"
+environ="-e DISPLAY -e HOME"
 volumes="-v ${workdir}:/local \
-        -v /home/${USER}/.ssh:/home/${USER}/.ssh \
-        -v /home/${USER}/.bash_history:/home/${USER}/.bash_history"
+        -v ${HOME}:${HOME}"
 opts="--net=host --rm -ti"
 x11opts="-v /dev/dri:/dev/dri --security-opt=label=type:container_runtime_t"
+identity="--security-opt=label=type:container_runtime_t --annotation run.oci.keep_original_groups=1 --userns=keep-id"
 
 set -x
-podman run ${path} ${environ} ${x11opts} ${volumes} ${@} ${opts} ${image}
+podman run ${path} ${environ} ${identity} ${x11opts} ${volumes} ${@} ${opts} ${image}
